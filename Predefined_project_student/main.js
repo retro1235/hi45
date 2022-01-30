18226bc37a7f8fde915d9f1300fa9fbba5eb74a9
retro1235 <82151737+retro1235@users.noreply.github.com>
2022-01-30 10:02:05 +0530

    Add files via upload

---
+++ Predefined_project_student/main.js
@@ -0,0 +1,50 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+    if(content == "1")
+    {
+        
+        document.getElementById("status").innerHTML = "Started drawing 1 apple";
+        draw_apple = "set";
+    }
+}
+
+
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    img = loadImage('apple.png');   
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
